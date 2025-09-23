3dc8093063ee43d7d078cd5fb99197b27e52c107
HerbertLu <3242473480@qq.com>
2025-09-23 22:18:59 +0800

    直接删掉拉倒，莫名其妙来一堆乱七八糟的文件，删掉一了百了！！！！！！！！！！

--- JavaWeb_LearningProject/SpringBootWeb02/src/main/java/com/itheima/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.itheima.dao;
-
-import java.util.List;
-
-public interface UserDao {
-    public List<String> findAll();
-}
--- JavaWeb_LearningProject/SpringBootWeb02/src/main/java/com/itheima/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.service;
-
-import com.itheima.pojo.User;
-
-import java.util.List;
-
-public interface UserService {
-    public List<User> findAll();
-}
